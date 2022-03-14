5ce07e01faf9d745c20b043e299c4fbbf300ad3a
kornyellow <kornkubzaza@gmail.com>
2022-03-14 15:30:11 +0700

    Done

--- 1.hpp
+++ 1.hpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
 	vector <int> twoSum(vector <int> &nums, int target) {
-		
 		int numsSize = nums.size();
 
 		unordered_map <int, int> answerMap;
--- 20.hpp
+++ 20.hpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
 	bool isValid(string s) {
-		
 		vector <char> queue;
-		
+
 		int ptr = 0;
 		int sSize = s.size();
 		while (ptr != sSize) {
@@ -11,12 +10,11 @@ public:
 			else if (s.at(ptr) == '{') queue.insert(queue.begin(), '}');
 			else if (s.at(ptr) == '[') queue.insert(queue.begin(), ']');
 			else {
-				if (queue.empty() || s.at(ptr) != queue.front()) return false; 
+				if (queue.empty() || s.at(ptr) != queue.front()) return false;
 				queue.erase(queue.begin());
 			}
 			ptr++;
 		}
-		
 		return queue.empty();
 	}
 };
